fbd4a2c235161306ac5df49c5d7af15289391754
nuttaporn kunsin <to_andata@hotmail.com>
2023-03-02 01:40:37 +0700

    initial project

---
+++ src/RunDotnetMicroServicesNuget.Common/Models/BaseResponseError.cs
@@ -0,0 +1,20 @@
+﻿namespace RunDotnetMicroServicesNuget.Common.Models
+{
+    public class BaseResponseError
+    {
+        public string Code { get; set; } = string.Empty;
+
+        public string Message { get; set; } = string.Empty;
+
+        public BaseResponseError()
+        {
+
+        }
+
+        public BaseResponseError(string code, string message)
+        {
+            this.Code = code;
+            this.Message = message;
+        }
+    }
+}
